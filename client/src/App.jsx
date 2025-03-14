4234166beadddd42e0312b2e5af6e3df1e28f54b
Anh Pham <quocanhxemer@gmail.com>
2025-03-14 15:15:51 +0100

    layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
-import Layout from './components/layout/layout'
+import Layout from './components/Layout'
 import { publicRoutes } from './routes'
 
 function App() {
--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -1,9 +1,8 @@
 import Header from "../Header";
-import classes from "./layout.module.css"
 import Leftbar from "../Leftbar";
 import Rightbar from "../rightbar/rightbar";
 
-import styles from "./layout.module.css"
+import styles from "./Layout.module.css"
 import classNames from "classnames/bind";
 
 const cls = classNames.bind(styles)
@@ -11,9 +10,7 @@ const cls = classNames.bind(styles)
 function Layout({ children }) {
   return (
     <div className={cls("layout")}>
-      <div className={classes.layoutHeader}>
-        <Header />
-      </div>
+      <Header />
       <div className={cls("layout-content")}>
         <Leftbar />
         {children}
