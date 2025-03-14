ed15cba2ebff70b22db344a712222b17c782ff87
Anh Pham <quocanhxemer@gmail.com>
2025-03-14 11:20:34 +0100

    restructure

--- client/src/components/Leftbar/Leftbar.jsx
+++ client/src/components/Leftbar/Leftbar.jsx
@@ -1,7 +1,11 @@
 import { Button } from "@mui/material"
-import "./leftbar.css"
 import ProfileListItem from "../ProfileListItem"
 import { Bookmark, Group, HelpOutline, PlayCircleFilledOutlined, RssFeed, School, WorkOutline, Event } from "@mui/icons-material"
+
+import styles from "./Leftbar.module.css"
+import classNames from "classnames/bind"
+
+const cls = classNames.bind(styles)
 
 const Icons = {
     Bookmark, Group, HelpOutline, PlayCircleFilledOutlined, RssFeed, School, WorkOutline, Event
@@ -47,23 +51,23 @@ export default function Leftbar() {
         item => {
             const Component = Icons[item.icon]
             return (
-                <li key={item.icon} className="leftbar-list-item">
-                    <Component className="leftbar-icon" />
-                    <span className="leftbar-list-item-text">{item.title}</span>
+                <li key={item.icon} className={cls("leftbar-list-item")}>
+                    <Component className={cls("leftbar-icon")} />
+                    <span className={cls("leftbar-list-item-text")}>{item.title}</span>
                 </li>
             )
         }
     )
 
     return (
-        <div className="leftbar">
-            <div className="leftbar-wrapper">
-                <ul className="leftbar-list">
+        <div className={cls("leftbar")}>
+            <div className={cls("leftbar-wrapper")}>
+                <ul className={cls("leftbar-list")}>
                     {renderItems(items)}
                 </ul>
-                <Button className="leftbar-button">Show more</Button>
-                <hr className="leftbar-hr" />
-                <div className="leftbar-friend-list">
+                <Button className={cls("leftbar-button")}>Show more</Button>
+                <hr className={cls("leftbar-hr")} />
+                <div className={cls("leftbar-friend-list")}>
                     <ProfileListItem profileImage={"profile_pics.jpg"} displayName="Name" />
                     <ProfileListItem profileImage={"profile_pics.jpg"} displayName="Name" />
                     <ProfileListItem profileImage={"profile_pics.jpg"} displayName="Name" />
--- client/src/pages/homepage/homepage.jsx
+++ client/src/pages/homepage/homepage.jsx
@@ -1,6 +1,6 @@
 import Feed from '../../components/Feed'
 import Rightbar from '../../components/rightbar/rightbar'
-import Leftbar from '../../components/leftbar/leftbar'
+import Leftbar from '../../components/Leftbar'
 import "./homepage.css"
 
 export default function Homepage() {
