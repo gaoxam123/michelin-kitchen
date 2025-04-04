e6cbcd2941bb5e28c39c74b80c96955c7008f017
kyredyrf nipohc <quocanhxemer@gmail.com>
2025-04-04 23:05:45 +0200

    ignore cookies at register/authenticate

--- server/src/main/java/backend/server/config/JwtAuthenticationFilter.java
+++ server/src/main/java/backend/server/config/JwtAuthenticationFilter.java
@@ -29,6 +29,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
+
+        if (request.getRequestURI().startsWith("/api/auth")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         Cookie[] cookies = request.getCookies();
 
         if (cookies == null) {
