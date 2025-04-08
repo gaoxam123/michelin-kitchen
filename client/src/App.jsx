4497a433f228bfb6d600f578866282d9552d1b6d
gaoxam123 <minhtengi1412@gmail.com>
2025-04-08 04:56:32 +0200

    refactor to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,42 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './App.css'
-import Layout from './components/Layout'
-import { publicRoutes } from './routes'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Layout from "./components/Layout";
+import { publicRoutes } from "./routes";
+import { useCallback, useEffect } from "react";
+import { auth } from "./store/user";
+import { useDispatch } from "react-redux";
 
 function App() {
+  const dispatch = useDispatch();
+  const authUser = useCallback(async () => {
+    await dispatch(auth());
+  }, [dispatch]);
+  useEffect(() => {
+    authUser();
+  }, [authUser]);
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          {publicRoutes.map(
-            (route, index) => {
-              const Component = route.component
-              const ComponentLayout = route.layout || Layout
-              return <Route
+          {publicRoutes.map((route, index) => {
+            const Component = route.component;
+            const ComponentLayout = route.layout || Layout;
+            return (
+              <Route
                 key={index}
                 path={route.path}
                 element={
                   <ComponentLayout>
                     <Component />
                   </ComponentLayout>
-                } />
-            }
-          )}
+                }
+              />
+            );
+          })}
         </Routes>
       </div>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
