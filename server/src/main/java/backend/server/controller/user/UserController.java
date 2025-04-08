edd0875bfb6693483abf61c8d541a35b6ba762eb
gaoxam123 <minhtengi1412@gmail.com>
2025-04-08 04:26:35 +0200

    fix endpoint

--- server/src/main/java/backend/server/controller/user/UserController.java
+++ server/src/main/java/backend/server/controller/user/UserController.java
@@ -13,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -48,6 +49,13 @@ public class UserController {
             return ResponseEntity.ok(blogService.findBlogsByUserIdSortByPostDate(id).stream().map(BlogResponse::new).toList());
         }
         return ResponseEntity.ok(blogService.findBlogsByUserId(id).stream().map(BlogResponse::new).toList());
+    }
+
+    @GetMapping("/users/auth-me")
+    public ResponseEntity<UserResponse> getCurrentUser(Authentication authentication) {
+        String username = authentication.getName();
+        System.out.println(username);
+        return ResponseEntity.ok(new UserResponse(userService.findByUsername(username)));
     }
 
     @PutMapping("/users")
