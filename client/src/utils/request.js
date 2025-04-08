187825da4d2e243a73dc74a72ab74fc60fa431f8
gaoxam123 <minhtengi1412@gmail.com>
2025-04-08 05:03:41 +0200

    add axios interceptor

--- client/src/utils/request.js
+++ client/src/utils/request.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import API from "./api";
+import routes from "../config/routes";
 
 // Spring app API
 // const API = "http://localhost:8080/api";
@@ -9,5 +10,16 @@ axios.defaults.withCredentials = true;
 const request = axios.create({
   baseURL: API,
 });
+
+// login whenever jwt is expired
+request.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 403) {
+      window.location.href = routes.login;
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default request;
