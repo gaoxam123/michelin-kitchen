366cccfc7b3f1f3e203f9fe8e02d4f0956c1c0a2
kyredyrf nipohc <quocanhxemer@gmail.com>
2025-04-16 19:19:56 +0200

    following list on leftbar

--- client/src/components/Leftbar/Leftbar.jsx
+++ client/src/components/Leftbar/Leftbar.jsx
@@ -1,26 +1,46 @@
 import Menu from "./Menu";
 import MenuItem from "./Menu/MenuItem";
 
+import { getProfilePictureURL } from "../../utils/getImages";
+
 import configRoutes from "../../config/routes";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faHouse,
-  faUserGroup,
-  faBookmark,
   faGear,
 } from "@fortawesome/free-solid-svg-icons";
 
-import { Button } from "@mui/material";
 import ProfileListItem from "../ProfileListItem";
 import { useSelector } from "react-redux";
 
 import styles from "./Leftbar.module.css";
 import classNames from "classnames/bind";
+import { useEffect, useState } from "react";
+import request from "../../utils/request";
+import apiRoutes from "../../config/apiRoutes";
+import routes from "../../config/routes";
 
 const cls = classNames.bind(styles);
 
 export default function Leftbar() {
-  const following = useSelector((state) => state.user.following);
+  const { user: logginUser, following } = useSelector((state) => state.user);
+
+  const [followedUsers, setFollowedUsers] = useState([])
+
+  useEffect(() => {
+    const fetchFollowedUsers = async (following) => {
+      const users = await Promise.all(
+        following.map(
+          async id =>
+            (await request.get(`${apiRoutes.users.base}/${id}`)).data
+        )
+      )
+
+      setFollowedUsers(users);
+    }
+
+    fetchFollowedUsers(following);
+  }, [following])
 
   const items = [
     {
@@ -33,18 +53,7 @@ export default function Leftbar() {
       icon: <FontAwesomeIcon icon={faGear} />,
       to: configRoutes.settings,
     },
-    {
-      title: "Groups",
-      icon: <FontAwesomeIcon icon={faUserGroup} />,
-      to: "/TODO",
-    },
-    {
-      title: "Bookmarks",
-      icon: <FontAwesomeIcon icon={faBookmark} />,
-      to: "TODO",
-    },
   ];
-  //   console.log(following);
 
   const renderMenu = () => (
     <Menu>
@@ -63,30 +72,17 @@ export default function Leftbar() {
     <div className={cls("leftbar")}>
       <div className={cls("leftbar-wrapper")}>
         {renderMenu()}
-
-        <Button className={cls("leftbar-button")}>Show more</Button>
         <hr className={cls("leftbar-hr")} />
+        <h3>Following profiles</h3>
         <div className={cls("leftbar-friend-list")}>
-          {/* {following.map((user) => (
+          {followedUsers.map((user) => (
             <ProfileListItem
               key={user.id}
-              // TODO: convert image
-              profileImage={null}
+              profileImage={getProfilePictureURL(user.id)}
               displayName={user.username}
+              to={`${routes.profile}/${user.id}`}
             />
-          ))} */}
-          <ProfileListItem
-            profileImage={"TODO: Add image"}
-            displayName="Name"
-          />
-          <ProfileListItem
-            profileImage={"TODO: Add image"}
-            displayName="Name"
-          />
-          <ProfileListItem
-            profileImage={"TODO: Add image"}
-            displayName="Name"
-          />
+          ))}
         </div>
       </div>
     </div>
