9fb81a829e81dac5b27a7201e13ab18398d28bb3
kyredyrf nipohc <quocanhxemer@gmail.com>
2025-04-20 11:44:43 +0200

    add sql init file

---
+++ init.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE IF NOT EXISTS michelin_db;
+
+CREATE USER IF NOT EXISTS 'michelin'@'%' IDENTIFIED BY 'michelin';
+GRANT ALL PRIVILEGES ON michelin_db.* TO 'michelin'@'%';
+FLUSH PRIVILEGES;
+
